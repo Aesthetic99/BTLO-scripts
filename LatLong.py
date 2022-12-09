5c90d4e2a0051ff5361849fbd142b75ad3892b89
Aesthetic99 <84500304+Aesthetic99@users.noreply.github.com>
2022-12-09 22:41:52 +0545

    Add files via upload

---
+++ LatLong.py
@@ -0,0 +1,119 @@
+# number = -0.841600711036728
+
+number = 5
+def divide(num):
+    return num/3
+
+def sub(num):
+    return num-3.14
+
+def mult(num):
+    return num*2*3*4*5
+
+def addition(num):
+    return num + 5
+
+funcList = [divide(number),sub(number),mult(number),addition(number)]
+
+print (funcList[3])
+
+
+# #divide
+# print (divide(sub(mult(addition(number)))))
+
+# print ("\n\n")
+
+# print(divide(sub(addition(mult(number)))))
+
+# print ("\n\n")
+
+# print (divide(mult(sub(addition(number)))))
+
+# print ("\n\n")
+
+# print(divide(mult(addition(sub(number)))))
+
+# print ("\n\n")
+
+# print (divide(addition(sub(mult(number)))))
+
+# print ("\n\n")
+
+# print (divide(addition(mult(sub(number)))))
+
+# print ("\n\n")
+
+# #sum
+# print (addition(sub(mult(divide(number)))))
+
+# print ("\n\n")
+
+# print(addition(sub(divide(mult(number)))))
+
+# print ("\n\n")
+
+# print (addition(mult(sub(divide(number)))))
+
+# print ("\n\n")
+
+# print(addition(mult(divide(sub(number)))))
+
+# print ("\n\n")
+
+# print (addition(divide(sub(mult(number)))))
+
+# print ("\n\n")
+
+# print (addition(divide(mult(sub(number)))))
+
+# print ("\n\n")
+
+# #subtraction
+# print (sub(divide(mult(addition(number)))))
+
+# print ("\n\n")
+
+# print(sub(divide(addition(mult(number)))))
+
+# print ("\n\n")
+
+# print (sub(mult(divide(addition(number)))))
+
+# print ("\n\n")
+
+# print(sub(mult(addition(divide(number)))))
+
+# print ("\n\n")
+
+# print (sub(addition(divide(mult(number)))))
+
+# print ("\n\n")
+
+# print (sub(addition(mult(divide(number)))))
+
+# print ("\n\n")
+
+# #mult
+# print (mult(sub(div(addition(number)))))
+
+# print ("\n\n")
+
+# print(mult(sub(addition(divide(number)))))
+
+# print ("\n\n")
+
+# print (mult(divide(sub(addition(number)))))
+
+# print ("\n\n")
+
+# print(mult(divide(addition(sub(number)))))
+
+# print ("\n\n")
+
+# print (mult(divide(sub(mult(number)))))
+
+# print ("\n\n")
+
+# print (mult(addition(divide(sub(number)))))
+
+# print ("\n\n")
